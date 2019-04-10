f942ff2e4a8b6573951b9da3b28d2638e3454821
ran0821 <luran0821@gmail.com>
2019-04-10 21:03:01 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import{ Provider } from 'react-redux'
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 
 import store from './store'
